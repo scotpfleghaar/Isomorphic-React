d3878551019baa9f39ac52c26aa3b9572a095c87
Scot <scot.pfleghaar@gmail.com>
2018-11-02 20:27:25 -0500

    Creating a simple react component to create a simple isomorphic react app.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
 
 });
 
-app.listen(port, ()=>{
-   console.log('Listening to port ' + port )
+app.listen(port, () => {
+    console.log('Listening to port ' + port)
 });
