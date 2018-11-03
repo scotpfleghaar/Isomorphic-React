2846d98cb2135d8fd5691d0633833416a3cb2a33
Scot <scot.pfleghaar@gmail.com>
2018-11-02 20:35:34 -0500

    Getting the require statements that the main react component.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-
+    const content = renderToString(<Home />);
+    res.send(content);
 });
 
 app.listen(port, () => {
