8fa866c062c1039e9e77e06535705d47b1adfaaf
Scot <scot.pfleghaar@gmail.com>
2018-11-03 21:39:24 -0500

    Setting up firebase project

---
+++ src/helpers/firestore.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyBDY2kyANcWweWIqtQLlrBn9_vVsfxPVCs",
+    authDomain: "isomorphic-react-75a22.firebaseapp.com",
+    databaseURL: "https://isomorphic-react-75a22.firebaseio.com",
+    projectId: "isomorphic-react-75a22",
+    storageBucket: "isomorphic-react-75a22.appspot.com",
+    messagingSenderId: "785454221019"
+};
+firebase.initializeApp(config);
+
+export default firebase;
