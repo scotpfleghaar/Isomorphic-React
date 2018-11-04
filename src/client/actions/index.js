3414c64d576151585a65dfa193d25dac8c900aea
Scot <scot.pfleghaar@gmail.com>
2018-11-04 08:04:33 -0600

    Making the call to get data in the actions.

---
+++ src/client/actions/index.js
@@ -0,0 +1,16 @@
+import db from '../../helpers/firestore';
+
+export const FETCH_USERS = 'FETCH_USERS';
+
+export const fetchUsers = () => async dispatch => {
+    db.collection('users').then(snapshot => {
+        snapshot.forEach(doc => {
+            dispatch({
+                type: FETCH_USERS,
+                payload:  doc.data()
+            })
+        });
+    }).catch(err => {
+        console.log('Error getting documents', err);
+    });
+};
