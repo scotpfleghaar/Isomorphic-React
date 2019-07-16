6ddac74a30491754c77ac587c02b15631aaab2f4
scot.pfleghaar <scot.pfleghaar@gmail.com>
2019-07-15 23:07:01 -0500

    Handling Styles

--- src/client/components/app/index.js
+++ src/client/components/app/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import Header from "../header";
 const Home = () => {
     return (
         <div>
+            <Header/>
             <span>I'm the main component</span>
             <button onClick={() => console.log('Activated')}>Press Me!</button>
+
+            <iframe width="600"
+                    height="600"
+                    src="https://ionicabizau.github.io/github-profile-languages/api.html?scotpfleghaar"
+                    frameBorder="0"/>
+
         </div>
     )
 };
---
+++ src/client/components/header/styles.js
@@ -0,0 +1,5 @@
+export const styles = {
+    navbar: {
+        backgroundColor: 'rgba(45,45,45,0.98)'
+    },
+};
